49ce175e12f2c0607ebac9f91953e25b60b22ceb
Marcela Linhares <marcelalgabilan@gmail.com>
2024-01-03 20:20:31 -0300

    atualização pensamento-computacional: codigosEmPython.py, Tema 1: módulo 3

---
+++ PrimeiroPeriodo/pensamento-computacional/codigosEmPython.py
@@ -0,0 +1,15 @@
+# TEMA 1: Introdução aos Princípios Fundamentais da Computação
+
+# Módulo 3: Código de instrução de computadores 
+#(Empregar códigos simples de instruções de computadores)
+
+print(6); # 6
+
+print("Bom dia",2,"Tchau"); # Bom dia 2 Tchau
+
+V=7;
+print(V); # 7
+print("O valor da variável V é:",V); # O valor da variável V é: 7
+
+NOME="João";
+print(NOME, "NOME", "NOME"); # João NOME NOME
