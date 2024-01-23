b9f4338e146b29bfd75fe8558e44f71742cbf60c
Marcela Linhares <marcelalgabilan@gmail.com>
2024-01-23 17:43:55 -0300

    atualização arquivo codigosEmJavascript na pasta pensamento-computacional

--- PrimeiroPeriodo/pensamento-computacional/codigosEmJavascript.js
+++ PrimeiroPeriodo/pensamento-computacional/codigosEmJavascript.js
@@ -11,3 +11,17 @@ pixel.setRed(255);                           // Ajustar para 255 o nível de ver
 pixel.setGreen(0);                           // Ajustar para 0 o nível de verde do pixel em questão
 pixel.setBlue(0);                            // Ajustar para 0 o nível de azul do pixel em questão
 console.log(img);                            // Apresenta imagem na tela
+
+// Módulo 2: Repetição for
+// (Distinguir a estrutura de repetição for)
+
+// Manipular todos os pixels de uma vez através da Repetição for (Loop)
+const img1 = new SimpleImage("passaro.jpg");
+
+for (let pixel of img1) {
+    pixel.setRed(0);
+    pixel.setGreen(0);
+    pixel.setBlue(0);
+}
+
+console.log(img1);
